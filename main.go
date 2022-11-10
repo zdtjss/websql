b06ae224141e2e37755fc3ace708f37e56acc96e
张德涛 <zhangdetao@shinho.net.cn>
2022-11-10 16:20:47 +0800

    参数化配置  支持检测启动状态

--- main.go
+++ main.go
@@ -1,30 +1,52 @@
 package main
 
 import (
+	"crypto/tls"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"go-web/https"
 	"go-web/sqlite"
 	"io"
+	"log"
 	"net/http"
+	"runtime"
 	"strings"
+	"time"
 )
 
-var httpClient = &http.Client{}
+var (
+	httpClient = &http.Client{}
+	port       *string
+	isHttps    *string
+)
 
 func main() {
+
+	port = flag.String("port", "443", "")
+	isHttps = flag.String("https", "true", "")
+	flag.Parse()
 
 	// 注册静态文件
 	fsRegister("/static/")
 	http.HandleFunc("/", mainHandler)
 	http.HandleFunc("/sqlite", dbTest)
 
-	// 初始化 TLS 证书
-	https.InitCertificateFile()
-	err := http.ListenAndServeTLS(":443", https.PemName, https.KeyName, nil)
+	// 检测是否启动成功
+	go sLsn()
+
+	var err error
+	if strings.EqualFold(*isHttps, "true") {
+		// 初始化 TLS 证书
+		https.InitCertificateFile()
+		err = http.ListenAndServeTLS(":"+*port, https.PemName, https.KeyName, nil)
+	} else {
+		err = http.ListenAndServe(":"+*port, nil)
+	}
 
 	if err != nil {
-		fmt.Println(err)
+		log.Println("********************* 系统启动失败，端口：" + *port + " *********************")
+		log.Println(err)
 	}
 
 }
@@ -89,4 +111,28 @@ func dbTest(w http.ResponseWriter, r *http.Request) {
 func fsRegister(baseUrl string) {
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle(baseUrl, http.StripPrefix(baseUrl, fs))
+}
+
+// 启动状态监听
+func sLsn() {
+	for {
+		// 2两秒后测试是否启动成功
+		time.Sleep(time.Duration(2 * time.Millisecond))
+		// 注意，使用 InsecureSkipVerify: true 来跳过证书验证，否则总是请求失败
+		client := &http.Client{Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		}}
+		protocol := "https"
+		if !strings.EqualFold(*isHttps, "true") {
+			protocol = "http"
+		}
+		r, _ := client.Get(protocol + "://localhost:" + *port)
+		if r != nil {
+			r.Body.Close()
+			log.Println("==================== 系统已启动完成，端口：" + *port + " ====================")
+			runtime.Goexit()
+		}
+	}
 }
