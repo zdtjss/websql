b46683e7a7a55fc1e68bdbc729436297c0fc1851
张德涛 <zhangdetao@shinho.net.cn>
2022-11-17 12:18:38 +0800

    完善

--- web-src/src/views/HomeView.vue
+++ web-src/src/views/HomeView.vue
@@ -2,13 +2,14 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import axios from 'axios'
+import dayjs from 'dayjs'
 
 const tableData = ref()
 
 onMounted(() => queryData())
 
 const queryData = () => {
-  axios.get('/api/sqlite')
+  axios.get("/api/sqlite")
     .then((response) => {
       tableData.value = response.data
     })
@@ -17,12 +18,15 @@ const queryData = () => {
     });
 }
 
+function formatterDateTime(row, column, cellValue, index) {
+  return dayjs(cellValue).format("YYYY-MM-DD")
+}
 </script>
 
 <template>
   <el-table :data="tableData" style="width: 100%">
-    <el-table-column prop="username" label="Date" width="180" />
-    <el-table-column prop="uepartment" label="Name" width="180" />
-    <el-table-column prop="created" label="Address" />
+    <el-table-column prop="username" label="名字" width="180" />
+    <el-table-column prop="uepartment" label="部门" width="180" />
+    <el-table-column prop="created" label="创建时间" :formatter="formatterDateTime" />
   </el-table>
 </template>
