bc5afd6780e3ff06f086ebebdaa22f896018d8e2
张德涛 <zhangdetao@shinho.net.cn>
2022-12-08 15:50:15 +0800

    通过首页地址获取父页面的参数

--- web-src/src/App.vue
+++ web-src/src/App.vue
@@ -1,6 +1,23 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
+for (const [key, value] of parsUrlVar()) {
+  sessionStorage.setItem(key, value)
+}
+
+function parsUrlVar() {
+  var paramMap = new Map();
+  var query = window.location.search;
+  if (query && query.charAt(0) === '?') {
+    let str = query.substring(1)
+    let params = str.split("&");
+    for (let i = 0; i < params.length; i++) {
+      paramMap.set(params[i].split("=")[0], decodeURI(params[i].split("=")[1]))
+    }
+  }
+  return paramMap
+}
 </script>
 
 <template>
