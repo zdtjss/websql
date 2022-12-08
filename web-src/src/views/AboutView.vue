77a3a4fe690f268d45c747ae346e8ab041593a04
张德涛 <zhangdetao@shinho.net.cn>
2022-12-08 16:19:27 +0800

    通过首页地址获取父页面的参数

--- web-src/src/views/AboutView.vue
+++ web-src/src/views/AboutView.vue
@@ -1,9 +1,49 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div style="display:block;">
+      <div>This is an test content {{ data.data }}</div>
+      <div v-if="data.ssv">{{ data.ssv }}</div>
+      <div v-if="data.lsv">{{ data.lsv }}</div>
+      <div v-if="data.pssv">{{ data.pssv }}</div>
+      <div v-if="data.plsv">{{ data.plsv }}</div>
+      <div v-if="data.pvar">{{ data.pvar }}</div>
+      <div v-if="data.token">{{ data.token }}</div>
+      <input type="button" @click="show" value="查看" />
+    </div>
   </div>
 </template>
 
+<script setup>
+
+import { reactive, onMounted } from 'vue'
+
+const data = reactive({
+  data: "哈哈",
+  ssv: "",
+  lsv: "",
+  pssv: "",
+  plsv: "",
+  pvar: "",
+  token: ""
+})
+
+
+onMounted(() => {
+  sessionStorage.setItem("ssk", "sessionStorage 值")
+  localStorage.setItem("lsk", "localStorage 值")
+}
+)
+
+function show() {
+  debugger
+  data.ssv = sessionStorage.getItem("ssk")
+  data.lsv = localStorage.getItem("lsk")
+  data.pvar = location.search
+  data.token = sessionStorage.getItem("token")
+  data.pssv = parent.sessionStorage.getItem("parent_ssk")
+  data.plsv = parent.localStorage.getItem("parent_lsk")
+}
+</script>
 <style>
 @media (min-width: 1024px) {
   .about {
