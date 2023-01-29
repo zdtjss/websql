993311a7068587401be2fbac289e41717b92c0f4
张德涛 <zhangdetao@shinho.net.cn>
2023-01-29 13:08:49 +0800

    优化打包

--- web-src/vite.config.ts
+++ web-src/vite.config.ts
@@ -17,8 +17,11 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('vue') || id.includes('pinia') || id.includes("element-plus")) {
+          if (id.includes('vue') || id.includes('pinia')) {
             return 'vue';
+          }
+          if (id.includes("element-plus")) {
+            return 'element-plus';
           }
           if (id.includes('node_modules')) {
             return 'vendor';
