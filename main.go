fbba37274596f22bda2f21e2275f0256ca639ffd
张德涛 <zhangdetao@shinho.net.cn>
2023-03-03 13:53:00 +0800

    优化代码

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 	port = flag.String("port", "80", "")
 	isHttps = flag.Bool("https", false, "")
 	initSqlFile := flag.String("sql", "", "")
-	isRemote := flag.Bool("remote", false, "true 使用redis存储token，适合多实例部署，默认false")
+	isRemote := flag.Bool("remote", false, "true 有严格的权限管理，适合多实例部署，默认false")
 	flag.Parse()
 
 	webapi.MainRegister(router)
@@ -48,34 +48,20 @@ func main() {
 		config.IsRemote = true
 	}
 
-	// 检测是否启动成功
-	go sLsn()
-
 	// https 默认端口 443
 	if *isHttps && *port == "80" {
 		*port = "443"
 	}
-	server := http.Server{Addr: ":" + *port, Handler: router}
+	server := &http.Server{Addr: ":" + *port, Handler: router}
 
 	// 启动服务
-	go func() {
-		var err error
-		if *isHttps {
-			// 初始化 TLS 证书
-			https.InitCertificateFile()
-			err = server.ListenAndServeTLS(https.PemName, https.KeyName)
-		} else {
-			err = server.ListenAndServe()
-		}
+	go startServer(server, isHttps, port)
 
-		if err != nil && !strings.Contains(err.Error(), "Server closed") {
-			log.Println("********************* 系统启动失败，端口：" + *port + " *********************")
-			log.Println(err)
-		}
-	}()
+	// 检测是否启动成功
+	go listenStartStatus()
 
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, os.Interrupt)
 	s := <-c
 	log.Printf("服务正在关闭 %s......", s)
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
@@ -86,11 +72,26 @@ func main() {
 	log.Println("服务已关闭")
 }
 
+func startServer(server *http.Server, isHttps *bool, port *string) {
+	var err error
+	if *isHttps {
+		// 初始化 TLS 证书
+		https.InitCertificateFile()
+		err = server.ListenAndServeTLS(https.PemName, https.KeyName)
+	} else {
+		err = server.ListenAndServe()
+	}
+	if err != nil && !strings.Contains(err.Error(), "Server closed") {
+		log.Println("********************* 启动失败，端口：" + *port + " *********************")
+		log.Println(err)
+		os.Exit(1)
+	}
+}
+
 // 启动状态监听
-func sLsn() {
+func listenStartStatus() {
 	for {
-		// 2两秒后测试是否启动成功
-		time.Sleep(time.Duration(2 * time.Millisecond))
+		time.Sleep(time.Duration(1 * time.Millisecond))
 		// 注意，使用 InsecureSkipVerify: true 来跳过证书验证，否则总是请求失败
 		client := &http.Client{Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{
@@ -101,10 +102,10 @@ func sLsn() {
 		if *isHttps {
 			protocol = "https"
 		}
-		r, _ := client.Get(protocol + "://localhost:" + *port)
+		r, _ := client.Get(strings.Join([]string{protocol, "://localhost:", *port, "/healthCheck"}, ""))
 		if r != nil {
 			r.Body.Close()
-			log.Println("==================== 系统已启动完成，端口：" + *port + " 、 https：" + strconv.FormatBool(*isHttps) + " ====================")
+			log.Println(strings.Join([]string{"==================== 系统已启动完成，端口：", *port, " 、 https：", strconv.FormatBool(*isHttps), " ===================="}, ""))
 			runtime.Goexit()
 		}
 	}
